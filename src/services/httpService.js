3cb83318d22b936fab5db5cdbf49291a14c133f1
balaclava <nebotxavi@gmail.com>
2020-01-15 11:33:42 +0100

    Deployment config

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+axios.defaults.baseURL = process.env.REACT_APP_API_URL; // https://hn.algolia.com/api/v1/
 
 axios.interceptors.response.use(null, error => {
   console.log(error);

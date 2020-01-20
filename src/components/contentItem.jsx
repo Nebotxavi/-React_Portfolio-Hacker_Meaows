9fcd081bc4d95dec98afdd32efe186f9f877f69e
balaclava <nebotxavi@gmail.com>
2020-01-20 23:03:04 +0100

    Links added to main content

--- src/components/contentItem.jsx
+++ src/components/contentItem.jsx
@@ -1,22 +1,30 @@
 import React from "react";
+
 import { getTimeAgo } from "../utils/getTimestamp";
+
+const linkEndpoint = "https://news.ycombinator.com/";
 
 const ContentItem = ({ items }) => {
   return (
     <div className="itemsList">
       {items.map(item => {
+        const authorLink = linkEndpoint + "user?id=" + item.author;
+        const contentLink = linkEndpoint + "item?id=" + item.objectID;
         if (item._tags[0] === "story")
           return (
             <div className="content-item" key={item.objectID}>
               <p>
-                {item.title}
-                <small>{item.url && `(${item.url})`}</small>
+                <a href={contentLink}>{item.title}</a>
+                <small>
+                  <a href={item.url}>{item.url && `(${item.url})`}</a>
+                </small>
               </p>
               <p>
                 <small>
-                  {item.points} | {item.author} |{" "}
-                  {getTimeAgo(item.created_at_i)} |
-                  {` ${item.num_comments} comments`}
+                  <a href={contentLink}>{item.points} points</a> |{" "}
+                  <a href={authorLink}>{item.author} </a>|
+                  <a href={contentLink}> {getTimeAgo(item.created_at_i)}</a> |
+                  <a href={contentLink}>{` ${item.num_comments} comments`}</a>
                 </small>
               </p>
             </div>

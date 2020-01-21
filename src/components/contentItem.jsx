eb01fd146718d3cbbad73de54f4872ca72515510
balaclava <nebotxavi@gmail.com>
2020-01-21 08:00:13 +0100

    Links added to the comments body

--- src/components/contentItem.jsx
+++ src/components/contentItem.jsx
@@ -3,13 +3,18 @@ import React from "react";
 import { getTimeAgo } from "../utils/getTimestamp";
 
 const linkEndpoint = "https://news.ycombinator.com/";
+const itemEndpoint = linkEndpoint + "item?id=";
 
 const ContentItem = ({ items }) => {
+  console.log(items);
   return (
     <div className="itemsList">
       {items.map(item => {
         const authorLink = linkEndpoint + "user?id=" + item.author;
-        const contentLink = linkEndpoint + "item?id=" + item.objectID;
+        const contentLink = itemEndpoint + item.objectID;
+        const parentLink = itemEndpoint + item.parent_id;
+        const threadLink = itemEndpoint + item.story_id;
+
         if (item._tags[0] === "story")
           return (
             <div className="content-item" key={item.objectID}>
@@ -34,9 +39,14 @@ const ContentItem = ({ items }) => {
             <div className="content-item" key={item.objectID}>
               <p>
                 <small>
-                  {item.points && item.points + " | "}
-                  {item.author} | {getTimeAgo(item.created_at_i)} | parent |
-                  {" " + item.story_title}
+                  <a href={contentLink}>
+                    {item.points && item.points + " points"}
+                  </a>{" "}
+                  {item.points && " | "}
+                  <a href={authorLink}>{item.author}</a> |{" "}
+                  <a href={contentLink}>{getTimeAgo(item.created_at_i)}</a> |{" "}
+                  <a href={parentLink}>parent</a> |{" on "}{" "}
+                  <a href={threadLink}>{item.story_title}</a>
                 </small>
               </p>
               <p>{item.comment_text}</p>
